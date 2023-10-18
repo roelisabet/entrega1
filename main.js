917b1b36a1e3809dfacdf4057727d39935b24af6
roelisabet <roelisabet5@gmail.com>
2023-10-18 03:19:20 -0300

    subiendo cambios

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function verificarFan() {
-    var fan = parseInt(prompt("Cuantas canciones de Taylor Swift conoces?"));
+    let fan = parseInt(prompt("Cuantas canciones de Taylor Swift conoces?"));
 
     if (fan >= 5) {
         var eresSwiftie = prompt("¿Te consideras Swiftie?");
@@ -60,4 +60,67 @@ while(dias >0){
     }
     
  }
- diaFuncion();
+ diaFuncion(); 
+
+ 
+function Producto(nombre, precio) {
+  this.nombre = nombre;
+  this.precio = precio;
+}
+
+const merchMensaje = prompt("Te dejamos la lista de la merch de la pelicula. Presiona Enter para continuar");
+
+const listaProductos = [
+  new Producto("remera", 2500),
+  new Producto("Pantalon", 4000),
+  new Producto("gorra", 600),
+  new Producto("buzo", 10000),
+];
+
+function mostrarProductos(precioMaximo) {
+    let mensaje = "Lista de Productos: \n";
+    listaProductos.filter(producto => producto.precio <= precioMaximo).forEach((producto, indice) => {
+      mensaje += `Producto ${indice + 1}: ${producto.nombre}, Precio: $${producto.precio}\n`;
+    });
+  
+    if (mensaje === "Lista de Productos: \n") 
+      alert( "No hay productos que cumplan con el filtro de precio máximo.");
+    
+  
+  }
+
+function mostrarProductos() {
+  let mensaje = "Lista de Productos: \n";
+  listaProductos.forEach((producto, indice) => {
+    mensaje += `Producto ${indice + 1}: ${producto.nombre}, Precio: $${producto.precio}\n`;
+  });
+  alert("Este es el precio  y los numeros de la merch de la pelicula:\n" + mensaje);
+}
+
+function calcularPrecioTotal() {
+  const precioTotal = listaProductos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+  alert(`Precio Total de la merch de la pelicula: $${precioTotal}`);
+}
+
+mostrarProductos(10000);
+
+const confirmCompra = prompt("¿Deseas comprar algún producto?");
+if (confirmCompra) {
+  const productoCompra = prompt("Ingresa el número del producto que deseas comprar");
+  if (productoCompra >= 1 && productoCompra <= listaProductos.length) {
+    const productoSeleccionado = listaProductos[productoCompra - 1];
+    alert(`Has elegido comprar ${productoSeleccionado.nombre} por $${productoSeleccionado.precio}`);
+    const confirmacionCompra = confirm("¿Deseas confirmar la compra?");
+    if (confirmacionCompra) {
+      alert("Compra realizada con éxito. ¡Gracias por tu compra!");
+    } else {
+      alert("Compra cancelada. Hasta luego.");
+    }
+  } else {
+    alert("Número de producto inválido, la compra ha sido cancelada.");
+  }
+} else {
+  alert("Gracias por visitarnos. ¡Hasta luego!");
+}
+
+calcularPrecioTotal();
